9e6ab58c07b0d056678893aaa985a7618e7958e0
tun <tun@72614f6a-d181-cc42-8733-494ef0fbf5a6>
2013-05-23 12:46:34 +0000

    “单位管理” 样式修改。

--- trunk/application/views/sys/unit.php
+++ trunk/application/views/sys/unit.php
@@ -1,38 +1,68 @@
-<h2 class="contentTitle">单位管理</h2>
-<div class="pageContent">
-    <form method="post" action="/sys/unit/update" class="pageForm required-validate" onsubmit="return validateCallback(this, navTabAjaxDone);">
-        <div class="pageFormContent" id="unitForm" layoutH="56">
-            <fieldset>
-                <legend>单位管理</legend>
-                <table>
-                    <?php foreach ($cfgs as $cfg) {
- ?>
-                        <tr>
-                            <td class="label" valign="top">
-                            <?php if ($cfg['cfg_desc']) {
- ?><a href="javascript:showNotice('notice{$var.code}');" title="{$lang.form_notice}"><img src="images/notice.gif" width="16" height="16" border="0" alt="{$lang.form_notice}" /></a><?php } ?>
-<?= $cfg['cfg_name'] ?>：
-                        </td>
-                        <td>
-<?php if ($cfg['cfg_type'] == 'text') { ?><input name="value[<?= $cfg['cfg_id'] ?>]" type="text" value="<?= $cfg['cfg_value'] ?>" size="40" /><?php } ?>
-<?php if ($cfg['cfg_type'] == 'textarea') { ?><textarea name="value[<?= $cfg['cfg_id'] ?>]" class="editor" cols="100" rows="11"><?= $cfg['cfg_value'] ?></textarea><?php } ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!--框架必需start-->
+<script type="text/javascript" src="/public/assets/js/jquery-1.4.js"></script>
+<script type="text/javascript" src="/public/assets/js/framework.js"></script>
+<link href="/public/assets/css/import_basic.css" rel="stylesheet" type="text/css"/>
+<link  rel="stylesheet" type="text/css" id="skin" prePath="/public/assets/" />
+<!--框架必需end-->
 
-                        </td>
-                    </tr>
+<!--修正IE6不支持PNG图start-->
+<script type="text/javascript" src="/public/assets/js/method/pngFix/supersleight.js"></script>
+<!--修正IE6不支持PNG图end-->
+
+<!--鼠标移入变色start-->
+<script>
+	$(function(){
+		$(".navIcon").hover(function(){
+			$(this).addClass("navIcon_hover");
+		},function(){
+			$(this).removeClass("navIcon_hover");
+		})
+	})
+</script>
+<!--鼠标移入变色end-->
+<body>
+<div class="position">
+	<div class="center">
+	<div class="left">
+	<div class="right">
+		<span>当前位置：主页 >> 单位管理 </span>
+	</div>	
+	</div>	
+	</div>
+</div>	
+<div id="scrollContent">
+    <div class="box2" panelTitle="单位管理" showStatus="false">
+    <table class="tableStyle" transMode="true">
+<?php foreach ($cfgs as $cfg) { ?>
+        <tr>
+            <td>
+                                
+    <?= $cfg['cfg_name'] ?>：
+            </td>
+            <td>
+    <?php if ($cfg['cfg_type'] == 'text') { ?>
+            <label for="value[<?= $cfg['cfg_id'] ?>]">
+                <input name="value[<?= $cfg['cfg_id'] ?>]" type="text" value="<?= $cfg['cfg_value'] ?>" style="width:500px" />
+    <?php } ?>
+            </label>
+    <?php if ($cfg['cfg_type'] == 'textarea') { ?>
+            <span class="float_left"><textarea name="value[<?= $cfg['cfg_id'] ?>]"  style="width:500px"><?= $cfg['cfg_value']?></textarea></span>
+    <?php } ?>
+    
+            </td>
+        </tr>
 <?php } ?>
-                </table>
-            </fieldset>
-        </div>
-        <div class="formBar">
-            <ul>
-                <li>
-                    <div class="buttonActive"><div class="buttonContent"><button type="submit">保存</button></div></div>
-                </li>
-                <li>
-                    <div class="button"><div class="buttonContent"><button type="button" onclick="navTab.closeCurrentTab()">取消</button></div></div>
-                </li>
-            </ul>
-        </div>
-
-    </form>
+        <tr>
+        	<td colspan="2">
+        		<input type="submit" value=" 提 交 "/>
+        		<input type="reset" value=" 重 置 "/>
+        	</td>
+        </tr>
+    </table>
+    </div>
 </div>
+</body>
+</html>
